a7d9b91fe81b91fdd1601d545ab321357f3f6d34
AakashSrinivasan03 <s.aakash3431@gmail.com>
2018-01-28 06:42:19 -0800

    Third commit

---
+++ P2/P2.java
@@ -0,0 +1,21 @@
+import syntaxtree.*;
+import visitor.*;
+
+public class P2 {
+   public static void main(String [] args) {
+      try {
+         Node root = new MiniJavaParser(System.in).Goal();
+         //System.out.println("Program type checked successfully");
+         GJDepthFirst temp=new GJDepthFirst();
+         root.accept(temp,null); // Your assignment part is invoked here.
+         root.accept(temp,null);
+         root.accept(temp,null);
+        //GJNoArguDepthFirst.s.get(0);
+      }
+      catch (ParseException e) {
+         System.out.println(e.toString());
+      }
+   }
+} 
+//export PATH="/home/manikandan/java/javacc-5.0/bin":$PATH
+//java -jar jtb132.jar minijava.jj
